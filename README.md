114c2ba9c41e31facfe90d473235fc00c1300149
eiyuu001 <dummy@example.com>
2026-01-27 01:46:10 +0900

    mod: Change the height unit to dB

--- README.md
+++ README.md
@@ -4,31 +4,32 @@
 2. z < `binarize_threshold_sigma_minus`σ or z > `binarize_threshold_sigma_plus`σ の部分を1, それ以外を0として二値化する.
 3. 二値化したデータを[ラベリング](https://docs.scipy.org/doc/scipy-1.17.0/reference/generated/scipy.ndimage.label.html)する.
 4. 上端と繋がっていない孤立した要素をノイズとみなして取り除く. 今は上下左右の4近傍で接続性を見ている.
-5. 最も高い山の高さを検出する, その高さが`f01_height_min`未満の場合はf01該当なしとする.
+5. 最も高い山の高さを検出する, その高さが`f01_height_min`未満の場合はf01該当なしとする. 山の高さは`top_power`を基準に計算する. 例えば`top_power`が0dBで山が-20dBまで伸びているとき, 山の高さは20dBになる.
 6. 山の最も高い部分の周波数をf01とする, 複数ある場合はphase shiftの絶対値が一番大きい部分を採用する.
-7. f01を含む山の一次モーメントを計算(各部分のphase shiftの絶対値を質量, 上端を支点とみなす)し, その値と`f01_moment_thresholds`から`quality_level`を算出する.
-8. f01から低周波方向に走査し, `f12_distance_min` ~ `f12_distance_max` の距離にあり, `f12_height_min` 以上の高さの山の中で最も周波数の高い山をf12とする. そのような山が存在しない場合はf12該当なしとする.
+7. f01を含む山の一次モーメントを計算(`top_power`と各部分のパワーの差を腕の長さ, phase shiftの絶対値を1dBあたりの質量とみなす)し, その値と`f01_moment_thresholds`から`quality_level`を算出する.
+8. f01から低周波方向に走査し, `f12_distance_min` ~ `f12_distance_max` の距離にあり, `f12_height_min` 以上の高さの山の中で最も周波数の高い山をf12とする. そのような山が存在しない場合はf12該当なしとする. 山の高さは`top_power`を基準に計算する.
 
 ## `quality_level`について
 
-f01の山の一次モーメントをm, `f01_moment_thresholds`を`[0.1, 60.0, 120.0, 170.0, 500.0]`とした場合, 以下のように`quality_level`を算出する.
+f01の山の一次モーメントをm, `f01_moment_thresholds`を`[0.1, 1750.0, 3600.0, 5000.0, 15000.0]`とした場合, 以下のように`quality_level`を算出する.
 
 ```
-quality_level = 0:         m <=   0.1
-quality_level = 1:   0.1 < m <=  60.0
-quality_level = 2:  60.0 < m <= 120.0
-quality_level = 3: 120.0 < m <= 170.0
-quality_level = 4: 170.0 < m <= 500.0
-quality_level = 5: 500.0 < m
+quality_level = 0:           m <=     0.1
+quality_level = 1:     0.1 < m <=  1750.0
+quality_level = 2:  1750.0 < m <=  3600.0
+quality_level = 3:  3600.0 < m <=  5000.0
+quality_level = 4:  5000.0 < m <= 15000.0
+quality_level = 5: 15000.0 < m
 ```
 
 ## 設定ファイル
 
 - `binarize_threshold_sigma_(plus/minus)`: phase shift値を二値化する際に使う閾値の(+/-)側.
-- `f01_height_min`: f01として採用されるのに必要な最低限の山の高さ. 単位はyの1目盛.
+- `top_power`: 山の高さやモーメントの値の計算時に基準とするパワーの値(dB). グラフの上端のパワーの値とすることを推奨.
+- `f01_height_min`: f01として採用されるのに必要な最低限の山の高さ(dB).
 - `f01_moment_thresholds`: quality_levelの算出に使う閾値. 詳しくは[`quality_level`について](#quality_levelについて)を参照.
 - `f12_distance_(min/max)`: f12が存在するはずのf01からの距離範囲. 単位はxの1目盛.
-- `f12_height_min`: f12として採用されるのに必要な最低限の山の高さ. 単位はyの1目盛.
+- `f12_height_min`: f12として採用されるのに必要な最低限の山の高さ(dB).
 
 ## インストール
 
--- src/main.py
+++ src/main.py
@@ -19,11 +19,12 @@ import scipy.ndimage
 class QubitResponseConfig:
     binarize_threshold_sigma_plus: float
     binarize_threshold_sigma_minus: float
-    f01_height_min: int
+    top_power: float
+    f01_height_min: float
     f01_moment_thresholds: Sequence[float]
     f12_distance_min: int
     f12_distance_max: int
-    f12_height_min: int
+    f12_height_min: float
 
     def __post_init__(self):
         self._validate()
@@ -35,8 +36,8 @@ class QubitResponseConfig:
         if self.binarize_threshold_sigma_minus >= 0:
             raise ValueError('binarize_thresholds_sigma_minus must be negative')
 
-        if self.f01_height_min < 1:
-            raise ValueError('f01_height_min must be >= 1')
+        if self.f01_height_min <= 0:
+            raise ValueError('f01_height_min must be > 0')
 
         if len(self.f01_moment_thresholds) == 0 or any(
             b <= a for a, b in itertools.pairwise(self.f01_moment_thresholds)
@@ -46,8 +47,8 @@ class QubitResponseConfig:
         if self.f12_distance_min < 0 or self.f12_distance_min > self.f12_distance_max:
             raise ValueError('bad f12 distance range')
 
-        if self.f12_height_min < 1:
-            raise ValueError('f12_height_min must be >= 1')
+        if self.f12_height_min <= 0:
+            raise ValueError('f12_height_min must be > 0')
 
 
 class QubitResponse:
@@ -78,9 +79,11 @@ class QubitResponse:
 
     @functools.cached_property
     def f01(self):
-        max_height = max(self.heights)
+        idx_max_height = np.argmax(self.heights)
+        max_height = self.heights[idx_max_height]
+        max_height_db = self.heights_db[idx_max_height]
 
-        if max_height < self.config.f01_height_min:
+        if max_height_db < self.config.f01_height_min:
             return None
 
         idx_y = len(self.zs) - max_height
@@ -91,7 +94,9 @@ class QubitResponse:
 
         frequency = cast(float, self.xs[idx_x])
         label = cast(int, self.zs_labeled[idx_y, idx_x])
-        moment = self.compute_moment(self.zs, self.zs_labeled, label)
+        moment = self.compute_moment(
+            self.zs, self.zs_labeled, self.levers, self.y_diffs, label
+        )
         quality_level = np.searchsorted(
             self.config.f01_moment_thresholds, moment, side='left'
         )
@@ -116,7 +121,7 @@ class QubitResponse:
             if self.config.f12_distance_min
             <= self.f01.idx_x - peak.x_end + 1
             <= self.config.f12_distance_max
-            and peak.height >= self.config.f12_height_min
+            and peak.height_db >= self.config.f12_height_min
         ]
 
         if not peaks:
@@ -126,7 +131,7 @@ class QubitResponse:
         peaks = [peak for peak in peaks if peak.height == max_height]
         peak = max(peaks, key=lambda p: p.x_end)
 
-        idx_y = len(self.zs) - peak.height
+        idx_y = self.zs.shape[0] - peak.height
         idx_x = np.argmax(abs(self.zs[idx_y][peak.x_start : peak.x_end])) + peak.x_start
         frequency = cast(float, self.xs[idx_x])
 
@@ -141,24 +146,42 @@ class QubitResponse:
         _peaks: list[QubitResponse.Peak] = []
         x_start: int | None = None
 
-        heights = itertools.chain([0], self.heights, [0])
-        for x, (height_prev, height) in enumerate(itertools.pairwise(heights)):
+        heights = zip(self.heights, self.heights_db)
+        heights = itertools.chain([(0, 0)], heights, [(0, 0)])
+        for x, ((height_prev, height_db_prev), (height, _)) in enumerate(
+            itertools.pairwise(heights)
+        ):
             if height > height_prev:
                 x_start = x
 
             if height < height_prev and x_start is not None:
-                _peaks.append(QubitResponse.Peak(x_start, x, height_prev))
+                _peaks.append(
+                    QubitResponse.Peak(x_start, x, height_prev, height_db_prev)
+                )
                 x_start = None
 
         return _peaks
 
     @functools.cached_property
-    def heights(self):
+    def heights(self) -> npt.NDArray[np.int64]:
         m = self.zs_labeled != 0
         first = np.argmax(m, axis=0)
         all_false = ~m.any(axis=0)
         first[all_false] = self.zs_labeled.shape[0]
         return self.zs_labeled.shape[0] - first
+
+    @functools.cached_property
+    def heights_db(self) -> npt.NDArray[np.float64]:
+        h_map = np.append(0.0, self.config.top_power - self.ys[::-1])
+        return h_map[self.heights]
+
+    @functools.cached_property
+    def levers(self) -> npt.NDArray[np.float64]:
+        return self.config.top_power - self.ys
+
+    @functools.cached_property
+    def y_diffs(self) -> npt.NDArray[np.float64]:
+        return np.diff(np.append(self.ys, self.config.top_power))
 
     def _validate_input(self):
         if self.zs.ndim != 2:
@@ -173,15 +196,24 @@ class QubitResponse:
             raise ValueError('xs/ys too short')
         if np.any(np.diff(self.xs) <= 0):
             raise ValueError('xs must be strictly increasing')
+        if np.any(np.diff(self.ys) <= 0):
+            raise ValueError('ys must be strictly increasing')
+        if self.config.top_power <= np.max(self.ys):
+            raise ValueError(
+                '`top_power` must be greater than the maximum value of ys.'
+            )
 
     @staticmethod
     def compute_moment(
-        zs: npt.NDArray[np.float64], zs_labeled: npt.NDArray[np.int32], label: int
+        zs: npt.NDArray[np.float64],
+        zs_labeled: npt.NDArray[np.int32],
+        levers: npt.NDArray[np.float64],
+        y_diffs: npt.NDArray[np.float64],
+        label: int,
     ) -> float:
-        ys, xs = np.where(zs_labeled == label)
-        weights = np.abs(zs[ys, xs])
-        lever = zs.shape[0] - ys - 0.5
-        return float(np.sum(weights * lever))
+        indices_y, indices_x = np.where(zs_labeled == label)
+        weights = np.abs(zs[indices_y, indices_x])
+        return float(np.sum(weights * levers[indices_y] * y_diffs[indices_y]))
 
     @staticmethod
     def standardize(zs: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
@@ -232,6 +264,7 @@ class QubitResponse:
         x_start: int
         x_end: int
         height: int
+        height_db: float
 
 
 def create_figure(data, zs=None):
@@ -294,7 +327,7 @@ def process_data(data, conf, image_dir_base=None, plot=False, json_output=False)
             qubit_idx = data['layout']['title']['text'][-3:]
 
             fig.write_image(
-                os.path.join(image_dir, f'qubit_{qubit_idx}_{moment:04}.png')
+                os.path.join(image_dir, f'qubit_{qubit_idx}_{moment:06}.png')
             )
 
             fig.write_image(
